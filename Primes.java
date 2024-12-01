35f0f08deffac645bd3da5dc7e5b45da94f53d16
Shakedflam <shakedflam@gmail.com>
2024-12-01 09:06:50 +0200

    Primes first

--- Primes.java
+++ Primes.java
@@ -1,5 +1,31 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int untilWhatNumber= Integer.parseInt(args[0]);
+        int breakingPoint= (int)Math.sqrt(untilWhatNumber);
+        boolean[] isPrimre= new boolean[untilWhatNumber+1];
+        for (int i=2; i<isPrimre.length; i++){
+            isPrimre[i]=true;
+        }
+        int prime=2;
+        while (prime<=breakingPoint+1){
+            for (int j=prime+1; j<isPrimre.length; j++){
+                if (j%prime==0 ){
+                    isPrimre[j]=false;
+                }
+            }
+            for (int k=prime+1; k<untilWhatNumber; k++){
+                if (isPrimre[k]==true){
+                    prime=k;
+                    break;
+                }
+            }
+        }
+        int howManyPrimes=0;
+        for (int j=2; j<isPrimre.length; j++){
+            if (isPrimre[j]==true){
+                howManyPrimes++;
+            }
+        }
+        System.out.println("There are "+howManyPrimes+" primes between 2 and "+untilWhatNumber+" ");
     }
 }
