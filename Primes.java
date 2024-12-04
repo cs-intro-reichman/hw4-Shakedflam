5681630e47f4cb716c5feb9f0b2dd1613b7355a5
Shakedflam <shakedflam@gmail.com>
2024-12-04 09:45:33 +0200

    Hey

--- Primes.java
+++ Primes.java
@@ -1,12 +1,12 @@
 public class Primes {
     public static void main(String[] args) {
-        int untilWhatNumber= Integer.parseInt(args[0]);
-        boolean[] isPrime= new boolean[untilWhatNumber+1];
-        for (int i=2; i<isPrime.length; i++){
-            isPrime[i]=true;
+        final int untilWhatNumber = Integer.parseInt(args[0]);
+        boolean [] isPrime = new boolean[untilWhatNumber + 1];
+        for (int i = 2; i < isPrime.length; i++) {
+            isPrime[i] = true;
         }
-        int prime=2;
-        while (prime*prime<=untilWhatNumber){
+        int prime = 2;
+        while (prime * prime <= untilWhatNumber) {
             if (isPrime[prime]) {
                 int multiple = prime * prime;
                 while (multiple <= untilWhatNumber) {
@@ -17,12 +17,15 @@ public class Primes {
             prime++;
         }
         System.out.println("Prime numbers up to " + untilWhatNumber + ":");
-        int howManyPrimes=0;
-        for (int j=2; j<isPrime.length; j++){
-            if (isPrime[j]==true){
-                howManyPrimes++;
+        int primeCounter = 0;;
+        for (int i = 0; i < isPrime.length; i++) {
+            if (isPrime[i] == true) {
+                System.out.println(i);
+                primeCounter++;
             }
         }
-        System.out.println("There are " + howManyPrimes + " primes between 2 and " + untilWhatNumber + " (" + ((howManyPrimes * 100) / untilWhatNumber) + "% are primes)");
+        System.out.println("There are " + primeCounter + " primes between 2 and " + untilWhatNumber + " (" + ((primeCounter * 100) / untilWhatNumber) + "% are primes)");
+
+
     }
 }
